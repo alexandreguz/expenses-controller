a8341fbc5e8399ced979caf61661fe008af92be7
Alexandre Guz <guzalexandre@gmail.com>
2023-05-28 19:36:40 +0300

    lifting data with props callback

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,22 @@
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import { useState } from "react";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020')
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear)
+    console.log(selectedYear)
+
+  }
+
     return (
+      <div>
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
             <ExpenseItem
               title={props.items[0].title}
               amount={props.items[0].amount}
@@ -26,6 +38,7 @@ const Expenses = (props) => {
               date={props.items[3].date}
             />
         </Card>
+        </div>
     );
 }
 
